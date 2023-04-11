ba6e3b2c3289460aae5874773555160532c1267f
“y-small-space” <jy.small.space@outlook.com>
2023-04-11 17:05:57 +0800

    luyou

--- AJAX/HTTP.md
+++ AJAX/HTTP.md
@@ -16,12 +16,23 @@ HTTP Chypertext transport protoco1） 协议『超文本传输协议』，协议
 ```
 ## 响应报文
 ```
-行：HTTP版本号 200 OK（响应字符串）
-头
+行: HTTP版本号 200 OK（响应字符串）
+例子：HTTP/1.1 200 OK
+头: Content-Type:text/html;charset=utf-8
+    Content-length:2048
+    Content-encoding:gzip
+空行:
+体: <html>
+        <head>
+        </head>
+        <body>
+            <h1>www</h1>
+        </body>
+    </html>
 ```
 响应状态码：
-* 404
-* 403
-* 401
-* 500
-* 200
+* 404 找不到
+* 403 forbidden没有权限
+* 401 未授权
+* 500 内部错误
+* 200 ok
--- node.js/express/2.express创建的基本web服务器.js
+++ node.js/express/2.express创建的基本web服务器.js
@@ -1,21 +1,26 @@
 //1.导入express
-const express = require("express");
+const express = require('express');
 //2.创建web服务器
 const app = express();
 
-//4.监听客户端的GET和POST请求，并向客户端响应具体的内容
+/* /4.监听客户端的GET和POST请求，并向客户端响应具体的内容
 app.get('/user',(res,req)=>{
     //调用express提供的res.send（）放发，向客户端响应一个JSON对象
     res.send({name:'zs',age:20,gender:'男'})
-})
-app.post('/user',(res,req)=>{
+}) */
+app.get('/server',(req,res)=>{
+    //设置响应头
+    res.setHeader('Access-Control-Allow-Origin','http://127.0.0.1:5500')
     //调用express提供的send()
+    //设置响应体
     res.send('请求成功')
-})
+});
 
-
+/* app.use(cors({
+    origin: 'http://127.0.0.1:5500',
+})); */
 
 //3.启动web服务器
-app.listen(8090,()=>{
-    console.log('express server running at http://127.0.0.1:8090')
+app.listen(8000,()=>{
+    console.log('express server running at http://127.0.0.1:8000')
 })

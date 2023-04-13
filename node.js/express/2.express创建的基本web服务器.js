07ab66bf6b106bb1b6e2bba7cde28393129434b4
“y-small-space” <jy.small.space@outlook.com>
2023-04-13 18:07:46 +0800

    AJAX

--- node.js/express/2.express创建的基本web服务器.js
+++ node.js/express/2.express创建的基本web服务器.js
@@ -39,18 +39,69 @@ app.all('/json-server',(req,res)=>{
     const data = {
         name : 'sa',
         age : 'dwiu' 
-    }
+    };
     
     let str = JSON.stringify(data);
 
     res.send(str);
 })
-
+app.get('/ie',(req,res)=>{
+    //设置响应头 允许跨域
+    res.setHeader('Access-Control-Allow-Origin','*');
+    //调用express提供的send()
+    //设置响应体
+    res.send('IE');
+})
+app.get('/delay',(req,res)=>{
+    //设置响应头 允许跨域
+    res.setHeader('Access-Control-Allow-Origin','*');
+    //调用express提供的send()
+    //设置响应体
+    setTimeout(()=>{
+        res.send('延时响应');
+    },3000)
+})
+/* app.get('/jQuery-server',(req,res)=>{
+    //设置响应头 允许跨域
+    res.setHeader('Access-Control-Allow-Origin','*');
+    //调用express提供的send()
+    //设置响应体
+    res.send("hello jQuery-server!");
+})
+app.post ('/jQuery-server',(req,res)=>{
+    //设置响应头 允许跨域
+    res.setHeader('Access-Control-Allow-Origin','*');
+    //调用express提供的send()
+    //设置响应体
+    res.send("hello jQuery-server!");
+}) */
+//jQuery 服务
+app.all('/jQuery-server',(req,res)=>{
+    //设置响应头 允许跨域
+    res.setHeader('Access-Control-Allow-Origin','*');
+    //响应头
+    res.setHeader('Access-Control-Allow-Headers','*'); 
+    //调用express提供的send()
+    //设置响应体
+    const data = {name:'a',age:'1'}
+    res.send(JSON.stringify(data))
+})
+//axios
+app.all('/Axios',(req,res)=>{
+    //设置响应头 允许跨域
+    res.setHeader('Access-Control-Allow-Origin','*');
+    //响应头
+    res.setHeader('Access-Control-Allow-Headers','*'); 
+    //调用express提供的send()
+    //设置响应体
+    const data = {name:'a',age:'1'}
+    res.send(JSON.stringify(data))
+})
 /* app.use(cors({
     origin: 'http://127.0.0.1:5500',
 })); */
 
 //3.启动web服务器
 app.listen(8000,()=>{
-    console.log('express server running at http://127.0.0.1:8000')
+    console.log('express server running at http://127.0.0.1:8000');
 })

037962a30e9d3fc681be8d68578e1f739081ccf1
“y-small-space” <jy.small.space@outlook.com>
2023-05-09 01:09:31 +0800

    router

---
+++ react/React-router/note1/src/pages/home/Messages/index.jsx
@@ -0,0 +1,15 @@
+export default function Messages(){
+    return (
+      <div>
+        <li list-me>
+          <a href="/message1">message001</a>
+        </li>
+        <li list-me>
+          <a href="/message1">message002</a>
+        </li>
+        <li list-me>
+          <a href="/message3">message003</a>
+        </li>
+      </div>
+    );
+}

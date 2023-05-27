55f69565c3b6b2194475753e517e0c495ec3d3a9
“y-small-space” <jy.small.space@outlook.com>
2023-05-27 20:11:57 +0800

    .

--- react/React-axios/try/src/App.jsx
+++ react/React-axios/try/src/App.jsx
@@ -2,7 +2,8 @@ import axios from 'axios';
 
 function App() {
   const getStudentDate = function(){
-    axios.get("http://localhost:5050/students").then(
+    
+    axios.get("http://localhost:3000/note").then(
       (response) => {
         console.log(response);
       },
@@ -14,7 +15,7 @@ function App() {
   
   return (
     <div>
-      <button onClick={()=>getStudentDate()}>点我获取学生数据</button>
+      <button onClick={()=>getStudentDate()}>点我获取数据</button>
     </div>
   )
 }

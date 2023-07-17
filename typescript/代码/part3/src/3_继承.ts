b915ac9999b25e31f97229ddbd97cc75d2370595
“y-small-space” <jy.small.space@outlook.com>
2023-07-17 22:41:28 +0800

    T

--- typescript/代码/part3/src/3_继承.ts
+++ typescript/代码/part3/src/3_继承.ts
@@ -1,4 +1,4 @@
-(function(){
+ (function(){
     // 定义一个Animal类
     class Animal{
         name:string;
@@ -20,11 +20,34 @@
      * Dog extends Animal
      *  - 此时，Animal 被称为父类，Dog 被称为子类
      *  - 使用继承后，子类将会拥有父类所有的方法和属性
+     *  - 通过继承可以将多个类中共有的代码写在一个父类中
+     *      这样只需要写一次即可让所有的子类都同时拥有父类中的属性
+     *      如果希望在子类中添加一些父类中没有的属性和方法，直接加
+     *  - 如果在子类中添加了和父类相同的方法，则子类方法会覆盖掉父类的方法
+     *      这种子类覆盖掉父类的形式，我们称为方法的重写
      */
     class Dog extends Animal{
-        
+        run(){
+            console.log(`${this.name}在跑`)
+        }
+
+        sayHello(): void {
+            console.log("wolf~")
+        }
     }
 
-     
+    class Cat extends Animal{
+
+    }
+    
+    const dog = new Dog('xiugou',1);
+    const cat = new Cat('xiumao',1);
+
+    console.log(dog);
+    dog.sayHello();
+    dog.run();  
+
+    console.log(cat);
+    cat.sayHello();
 
 })();

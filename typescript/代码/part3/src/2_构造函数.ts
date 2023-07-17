ca0d1def124879fb95e54140ba31ce6274162144
“y-small-space” <jy.small.space@outlook.com>
2023-07-17 19:41:01 +0800

    T

---
+++ typescript/代码/part3/src/2_构造函数.ts
@@ -0,0 +1,19 @@
+class Dog{
+    name : string;
+    age : number;
+
+    // constructor 被称为构造函数
+    // 构造函数会在对象创建是调用
+    constructor(name:string,age:number){
+        // 在实例对象中this就是表示当前新建的那个对象
+        this.name = name;
+        this.age = age
+    }
+    
+    bark(){
+        alert("wolf~")
+    }
+}
+
+const dog = new Dog("123",123)
+console.log(dog)

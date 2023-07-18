6287533c02fb68e7dd0cb5c210a79555b4108adc
“y-small-space” <jy.small.space@outlook.com>
2023-07-18 15:09:25 +0800

    W

---
+++ Webpack/webpack_docs/docs/.vuepress/.temp/internal/clientAppRootComponents.js
@@ -0,0 +1,5 @@
+import clientAppRootComponent0 from '/Users/gjy/Desktop/Firstnote/Webpack/webpack_docs/node_modules/@vuepress/plugin-back-to-top/lib/client/components/BackToTop.js'
+
+export const clientAppRootComponents = [
+  clientAppRootComponent0,
+]

c55915672bb87ae81e1ca0faeb520202a4ba4111
“y-small-space” <jy.small.space@outlook.com>
2023-09-20 02:29:58 +0800

    Vue

---
+++ VUE/03/zujian/First_Vue/src/components/TodoHeader.vue
@@ -0,0 +1,39 @@
+<template>
+  <header class="header">
+    <h1>Todo List</h1>
+    <input
+      class="new-todo"
+      placeholder="添加新任务"
+      v-model="newTask"
+      @keyup.enter="addTask"
+    />
+  </header>
+</template>
+  
+  <script>
+export default {
+  data() {
+    return {
+      newTask: "",
+    };
+  },
+  methods: {
+    addTask() {
+      if (this.newTask.trim() === "") return;
+      this.$emit("add", this.newTask.trim());
+      this.newTask = "";
+    },
+  },
+};
+</script>
+
+<style>
+.header{
+    height: 100px;
+    background-color: slategray;
+    text-align: center;
+}
+input{
+    height: 40px;
+}
+</style>
---
+++ VUE/03/zujian/First_Vue/src/components/Todomain.vue
@@ -0,0 +1,46 @@
+
+<!-- Todomain.vue -->
+<template>
+  <section class="main">
+    <ul class="todo-list">
+      <li v-for="(item, index) in list" :key="item.id" class="todo">
+        <div class="view">
+          <span class="index">{{ index + 1 }}</span
+          >.<label>{{ item.name }}</label>
+          <button class="destroy" @click="handleDel(item.id)">删除</button>
+        </div>
+      </li>
+    </ul>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    list: Array,
+  },
+  methods: {
+    deleteTask(taskId) {
+      // 触发删除任务的事件，将任务 ID 发送给父组件
+      this.$emit("delete", taskId);
+    },
+    handleDel(id) {
+      this.$emit('del',id)
+    },
+  },
+};
+</script>
+
+<style scoped>
+.main {
+  width: 350px;
+  height: 400px;
+  background-color: aquamarine;
+  color: #000;
+  text-decoration: none;
+}
+button {
+  float: right;
+  margin: 0 10px 0 0;
+}
+</style>
